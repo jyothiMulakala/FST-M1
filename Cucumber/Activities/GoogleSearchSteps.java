aa5d1f8e811c78c31a315e469fdebee9db9829eb
jyothiMulakala <jyothi.mulakala@ibm.com>
2025-03-13 20:22:41 +0530

    Add files via upload

--- Cucumber/Activities/GoogleSearchSteps.java
+++ Cucumber/Activities/GoogleSearchSteps.java
@@ -1,42 +1,30 @@
 package stepDefinitions;
 
-import io.cucumber.java.en.And;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-public class GoogleSearchSteps {
-    WebDriver driver;
-    WebDriverWait wait ;
-
-    @Given("^User is on Google homePage$")
-     public void userIsOnGooglePage(){
-      driver = new FirefoxDriver();
-      wait = new WebDriverWait(driver , Duration.ofSeconds(15));
-//open the browser
-      driver.get("https://google.com");
-    }
-
-    @When("^User types in Cheese and hits ENTER$")
-    public void userTypesInCheese(){
-        driver.findElement(By.name("q")).sendKeys("Cheese", Keys.RETURN);
-    }
-    @Then("^Show how many search results were shown$")
-    public void showHowManySearchResults(){
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("result-stats")));
-        String result = driver.findElement(By.id("result-stats")).getText();
-        System.out.println("The No. of Results :" + result);
-    }
-    @And("^Close the browser$")
-    public void closeBrowser(){
-        driver.close();
-    }
+public class GoogleSearchSteps extends BaseClass{
+	@Given("User is on the Google homepage")
+	public void userIsOnGooglePage() throws Throwable {
+		//Open the browser
+		driver.get("https://www.google.com");
+	}
+	
+	@When("they type Cheese and hits ENTER")
+	public void userTypesInCheeseHitEnter() throws Throwable  {
+		driver.findElement(By.name("q")).sendKeys("Cheese", Keys.ENTER);
+	}
+	
+	@Then("it should show how many Search results were found")
+	public void searchResultsDisplay() throws Throwable {
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("hdtb-tls"))).click();
+		String resultStats = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("result-stats"))).getText();
+		System.out.println("Number of results found: " + resultStats);
+	}
+	
 }
